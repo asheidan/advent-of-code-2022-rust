7aba1bad543040743376067c2afb870d7e4a7c2d
Emil Eriksson <emil.eriksson@codemill.se>
2022-12-04 12:00:42 +0100

    Cleanup of solutions for Day 04

--- src/bin/day04.rs
+++ src/bin/day04.rs
@@ -2,47 +2,59 @@
 use std::io::BufRead;
 use std::ops::RangeInclusive;
 
+trait ContainsAll<T> {
+    fn contains_all(&self, other: T) -> bool;
+}
 
-fn range_contains(first: &RangeInclusive<i32>, other: &RangeInclusive<i32>) -> bool {
-    other.clone().all(|n| first.contains(&n))
+impl ContainsAll<&RangeInclusive<i32>> for RangeInclusive<i32> {
+    fn contains_all(&self, other: &RangeInclusive<i32>) -> bool {
+        self.contains(other.start()) && self.contains(other.end())
+    }
+}
+
+trait Overlaps {
+    fn overlaps(&self, other: &Self) -> bool;
+}
+
+impl Overlaps for RangeInclusive<i32> {
+    fn overlaps(&self, other: &Self) -> bool {
+        self.contains(other.start())
+            || self.contains(other.end())
+            || other.contains(self.start())
+            || other.contains(self.end())
+    }
 }
 
 /// Full containment of one or the other
-fn solution_a(data: &[Vec<i32>]) -> i32 {
+fn solution_a(data: &[(RangeInclusive<i32>, RangeInclusive<i32>)]) -> i32 {
     data.iter()
-        .map(|assignments| match assignments[..] {
-            [first_start, first_end, second_start, second_end] => {
-                (first_start..=first_end, second_start..=second_end)
-            }
-            _ => panic!(""),
-        })
-        .filter(|(first, second)| range_contains(first, second) || range_contains(second, first))
+        .filter(|(first, second)| first.contains_all(second) || second.contains_all(first))
         .count() as i32
 }
 
 /// Any overlap
-fn solution_b(data: &[Vec<i32>]) -> i32 {
+fn solution_b(data: &[(RangeInclusive<i32>, RangeInclusive<i32>)]) -> i32 {
     data.iter()
-        .map(|assignments| match assignments[..] {
-            [first_start, first_end, second_start, second_end] => {
-                (first_start..=first_end, second_start..=second_end)
-            }
-            _ => panic!(""),
-        })
-        .filter(|(first, second)| first.clone().any(|n| second.contains(&n)))
+        .filter(|(first, second)| first.overlaps(second))
         .count() as i32
 }
 
 fn main() {
     let stdin = std::io::stdin();
-    let data: Vec<Vec<i32>> = stdin
+    let data: Vec<(RangeInclusive<i32>, RangeInclusive<i32>)> = stdin
         .lock()
         .lines()
         .map(|line| {
             line.unwrap()
                 .split(|c: char| c == '-' || c == ',')
                 .map(|n| n.parse::<i32>().unwrap())
-                .collect()
+                .collect::<Vec<i32>>()
+        })
+        .map(|assignments: Vec<i32>| match assignments[..] {
+            [first_start, first_end, second_start, second_end] => {
+                (first_start..=first_end, second_start..=second_end)
+            }
+            _ => panic!(""),
         })
         .collect();
 
