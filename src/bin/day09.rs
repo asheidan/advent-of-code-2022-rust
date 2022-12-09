f4be8ded1299639e56276f46443411ebf3285b3d
Emil Eriksson <asheidan@gmail.com>
2022-12-09 23:41:41 +0100

    Add working solution for Day 09 B

--- src/bin/day09.rs
+++ src/bin/day09.rs
@@ -1,6 +1,7 @@
 use std::collections::HashSet;
 use std::io::BufRead;
 use std::ops::AddAssign;
+use std::fmt::Display;
 
 #[derive(Debug, Clone, Copy)]
 enum Direction {
@@ -38,6 +39,18 @@ impl Position {
     fn is_touching(&self, other: &Self) -> bool {
         (self.x - other.x).abs() <= 1 && (self.y - other.y).abs() <= 1
     }
+
+    fn closer(&self, other: &Self) -> Self {
+        let x = ((self.x + other.x * 2) as f32 / 3.0).round() as i32;
+        let y = ((self.y + other.y * 2) as f32 / 3.0).round() as i32;
+        Self { x , y }
+    }
+}
+
+impl Display for Position {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({},{})", self.x, self.y)
+    }
 }
 
 impl AddAssign<Step> for Position {
@@ -58,6 +71,49 @@ impl From<Direction> for Step {
     }
 }
 
+fn solve_b(data: &[(Direction, u8)]) -> i32 {
+    let mut visited: HashSet<Position> = HashSet::new();
+
+    let mut knots: Vec<Position> = (0..10).map(|_| Position{x:0, y:0}).collect();
+
+    visited.insert(knots[9]);
+
+    for (direction, steps) in data.iter() {
+        //eprintln!("{:2} {:?}", steps, direction);
+        let step = Step::from(*direction);
+
+        for _ in 0..*steps {
+            knots[0] += step;
+
+            (1..(knots.len()))
+                .for_each(|index| {
+                    let other = knots[index - 1];
+                    let this = knots[index];
+
+                    if ! this.is_touching(&other) {
+                        //eprintln!("not touching!!! {} <> {}", this, other);
+                        let new_pos = this.closer(&other);
+                        //eprintln!("new: {}", new_pos);
+                        knots[index].x = new_pos.x;
+                        knots[index].y = new_pos.y;
+                    }
+                });
+
+            /*
+            for p in knots.iter() {
+                eprint!("{},", p);
+            }
+            eprintln!();
+            */
+
+            visited.insert(knots[9]);
+        }
+
+    }
+
+    return visited.len() as i32
+}
+
 fn solve_a(data: &[(Direction, u8)]) -> i32 {
     let mut visited: HashSet<Position> = HashSet::new();
 
@@ -68,16 +124,16 @@ fn solve_a(data: &[(Direction, u8)]) -> i32 {
     visited.insert(tail);
 
     for (direction, steps) in data.iter() {
-        eprintln!("{:2} {:?}", steps, direction);
+        //eprintln!("{:2} {:?}", steps, direction);
         let step = Step::from(*direction);
         for _ in 0..*steps {
             head += step;
-            eprintln!("tail: {:?} | head: {:?}", tail, head);
+            //eprintln!("tail: {:?} | head: {:?}", tail, head);
 
             if ! tail.is_touching(&head) {
                 // Tail need to move closer, cheat and use previous position of head
                 tail = previous_head.clone();
-                eprintln!("tail moved to: {:?}", tail);
+                //eprintln!("tail moved to: {:?}", tail);
                 visited.insert(tail);
             }
 
@@ -104,6 +160,7 @@ fn main() {
         .collect();
 
     println!("A: {}", solve_a(&data));
+    println!("B: {}", solve_b(&data));
 }
 
 #[cfg(test)]
@@ -168,5 +225,53 @@ mod tests {
                 assert_eq!(false, result);
             }
         }
+
+        mod closer {
+            use super::Position;
+
+            #[test]
+            fn east_east_position_should_move_east() {
+                // Given
+                let position = Position { x: 4, y: 4 };
+                let other = Position { x: 6, y: 4 };
+
+                // When
+                let result = position.closer(&other);
+
+                // Then
+                let expected = Position { x: 5, y: 4 };
+                assert_eq!(expected, result);
+            }
+
+
+            #[test]
+            fn north_north_position_should_move_north() {
+                // Given
+                let position = Position { x: 4, y: 4 };
+                let other = Position { x: 4, y: 6 };
+
+                // When
+                let result = position.closer(&other);
+
+                // Then
+                let expected = Position { x: 4, y: 5 };
+                assert_eq!(expected, result);
+            }
+
+            #[test]
+            fn north_northeast_position_should_move_northeast() {
+                // Given
+                let position = Position { x: 4, y: 4 };
+                let other = Position { x: 5, y: 6 };
+
+                // When
+                let result = position.closer(&other);
+
+                // Then
+                let expected = Position { x: 5, y: 5 };
+                assert_eq!(expected, result);
+            }
+        }
     }
 }
+
