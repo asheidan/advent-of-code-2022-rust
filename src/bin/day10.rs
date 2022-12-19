5ff3bc40edc2dc8600cfeb01077f35744559966a
Emil Eriksson <asheidan@gmail.com>
2022-12-19 21:38:09 +0100

    Cleanup of solution for Day 10 A

--- src/bin/day10.rs
+++ src/bin/day10.rs
@@ -1,9 +1,19 @@
+use std::fmt;
 use std::io::BufRead;
 
 #[derive(Debug)]
 enum Instruction {
     Noop,
     AddX(i32),
+}
+
+impl fmt::Display for Instruction {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Instruction::Noop => f.write_fmt(format_args!("nop")),
+            Instruction::AddX(v) => f.write_fmt(format_args!("add {}", v)),
+        }
+    }
 }
 
 impl From<&String> for Instruction {
@@ -17,41 +27,109 @@ impl From<&String> for Instruction {
     }
 }
 
-fn should_accumulate(cycle: i32) -> i32 {
-    if cycle % 40 == 20 {
-        eprintln!("------------accumulate!!!");
-        cycle
-    } else {
-        0
+struct Device<'i> {
+    cycle_counter: i32,
+    register_x: i32,
+
+    instruction_cycle: i32,
+
+    program_counter: i32,
+    instructions: &'i [Instruction],
+}
+
+impl<'i> Device<'i> {
+    fn new(instructions: &'i [Instruction]) -> Device<'i> {
+        let program_counter = -1;
+
+        Self {
+            cycle_counter: 0,
+            register_x: 1,
+
+            instruction_cycle: 1,
+
+            program_counter,
+            instructions,
+        }
+    }
+
+    fn cycles_for(instruction: &Instruction) -> i32 {
+        match *instruction {
+            Instruction::Noop => 1,
+            Instruction::AddX(_) => 2,
+        }
+    }
+
+    fn current_instruction(&self) -> &'i Instruction {
+        self.instructions
+            .get(self.program_counter as usize)
+            .unwrap()
+    }
+}
+
+impl<'i> Iterator for Device<'i> {
+    type Item = i32;
+
+    /// Returns in the cycle
+    ///
+    /// Returns -v
+    /// ____ ____ _
+    ///  0  V   1  V
+    ///     |      |
+    ///
+    fn next(&mut self) -> Option<i32> {
+        let mut loaded_instruction = false;
+        // End of previous cycle
+        // 0. End cycle
+        self.instruction_cycle -= 1;
+
+        // 1. Perform changes from operation
+        if self.instruction_cycle == 0 {
+            match self.instructions.get(self.program_counter as usize) {
+                Some(&Instruction::AddX(value)) => self.register_x += value,
+                Some(&Instruction::Noop) => (),
+                None => eprintln!("No instruction: pc: {}", self.program_counter),
+            }
+        }
+
+        // Beginning of cycle
+        self.cycle_counter += 1;
+
+        if self.instruction_cycle == 0 {
+            self.program_counter += 1;
+            if self.program_counter as usize >= self.instructions.len() {
+                return None;
+            }
+
+            self.instruction_cycle = Self::cycles_for(self.current_instruction());
+            loaded_instruction = true;
+        }
+
+        if loaded_instruction {
+            eprintln!(
+                "pc:{:3} | x:{:3} | {}",
+                self.cycle_counter,
+                self.register_x,
+                self.current_instruction());
+        } else {
+            eprintln!(
+                "pc:{:3} | x:{:3} |",
+                self.cycle_counter,
+                self.register_x
+                );
+        }
+
+        Some(self.cycle_counter)
     }
 }
 
 fn solve_a(data: &[Instruction]) -> i32 {
-    let mut cycle = 0;
-    let mut x = 1;
-
+    let mut device = Device::new(data);
     let mut sum = 0;
 
-    for instruction in data.iter() {
-        eprintln!("-- {:?}", instruction);
-        match instruction {
-            Instruction::Noop => {
-                cycle += 1;
-                sum += should_accumulate(cycle) * x;
-            },
-            Instruction::AddX(value) => {
-                cycle += 1;
-                sum += should_accumulate(cycle) * x;
-                eprintln!("C:{:4} |", cycle);
-
-                cycle += 1;
-                sum += should_accumulate(cycle) * x;
-
-                x += value;
-            }
-        };
-        eprintln!("C:{:4} | X: {}", cycle, x);
-
+    while let Some(cycle) = device.next() {
+        if cycle % 40 == 20 {
+            sum += cycle * device.register_x;
+        }
     }
 
     sum
