88f4dd52ba315037e3aecbf3ef238c084825c546
Emil Eriksson <asheidan@gmail.com>
2022-12-19 13:25:23 +0100

    Working solution for Day 07 B

--- src/bin/day07.rs
+++ src/bin/day07.rs
@@ -2,14 +2,16 @@ use std::collections::HashMap;
 use std::io::Read;
 
 fn solve_a(files: &[(String, usize)]) -> usize {
+    // Map of directory path to size of files contained in the directory
+    // This does not include the size of subdirectories
     let mut dir_sizes: HashMap<String, usize> = HashMap::new();
     let mut result = 0;
 
     for (path, size) in files.iter().rev() {
-        eprintln!("{} {}", path, size);
+        //eprintln!("{} {}", path, size);
         if 0 != *size {
             if let Some((dir_path, _file)) = path.rsplit_once("/") {
-                eprintln!("{}", dir_path);
+                //eprintln!("{}", dir_path);
                 // Update entry in HashTable
                 let entry = dir_sizes.entry(dir_path.to_string());
                 *(entry.or_default()) += *size;
@@ -31,9 +33,52 @@ fn solve_a(files: &[(String, usize)]) -> usize {
         }
     }
 
-    eprintln!("{:?}", dir_sizes);
-
     result
+}
+
+fn solve_b(files: &[(String, usize)]) -> usize {
+    let fs_limit = 70000000;
+    let space_needed = 30000000;
+
+    let used_space: usize = files.iter().map(|(_path, size)| size).sum();
+    //eprintln!("used space:{:9}", used_space);
+
+    let remove_space = space_needed - (fs_limit - used_space);
+    //eprintln!("need space:{:9}", remove_space);
+
+    let dir_sizes = directory_sizes(files);
+
+    /*
+    for (path, size) in &dir_sizes {
+        if *size >= remove_space && (*size - remove_space) < 100000 {
+            eprintln!("\x1b[33;1m{:9} : {:9} : {}\x1b[0m", size, size - remove_space, path);
+        } else {
+            eprintln!("{:9} : {:9} : {}", size, "", path);
+        }
+    }
+    */
+
+    let answer = dir_sizes.iter()
+        .map(|(_p,s)| s.clone())
+        .filter(|&size| size >= remove_space)
+        .min()
+        .unwrap();
+    //eprintln!("remo space:{:9}", answer);
+
+    answer
+}
+
+fn directory_sizes(files: &[(String, usize)]) -> Vec<(String, usize)> {
+
+    let dir_sizes: Vec<(String, usize)> = files.iter().filter(|(_path, size)| *size == 0).map(|(dir_path, _size)| {
+        let dir_size: usize = files.iter()
+            .filter_map(|(file_path, size)| if file_path.starts_with(dir_path) { Some(*size) } else { None })
+            .sum();
+
+        (dir_path.clone(), dir_size)
+    }).collect();
+
+    dir_sizes
 }
 
 fn path_from(dir_stack: &[String], file_name: &str) -> String {
@@ -54,8 +99,6 @@ fn main() {
         .read_to_string(&mut buffer)
         .expect("Reading from stdin");
 
-    //let mut root_dir = Directory::new();
-    //let mut current_dir = &mut root_dir;
     let mut files: Vec<(String, usize)> = Vec::new();
 
     let mut dir_stack: Vec<String> = Vec::new();
@@ -70,19 +113,13 @@ fn main() {
             }
         });
         let command_line = iterator.next().unwrap().split(" ").collect::<Vec<&str>>();
-        //eprintln!("cmd: {:?}", command_line);
         match &command_line[..] {
             &["cd", ".."] => {
                 dir_stack.pop();
-                //current_dir = &mut root_dir;
-                //current_dir = &mut root_dir;
-                //dir_stack.iter().for_each(|dir_name| current_dir = current_dir.subdirs.get_mut(dir_name).unwrap());
-                //dir_stack.iter().fold(&root_dir, |current_dir, dir_name| current_dir.subdirs.get(dir_name).unwrap());
             }
 
             &["cd", dir_name] => {
                 dir_stack.push(dir_name.to_string());
-                //current_dir = current_dir.subdirs.get_mut(&dir_name.to_string()).unwrap();
             }
 
             &["ls"] => {
@@ -94,7 +131,6 @@ fn main() {
                             files.push((path, 0));
                         }
                         (Some(size_string), Some(file_name)) => {
-                            //current_dir .files .push(size_string.parse::<usize>().unwrap());
                             let path = path_from(&dir_stack, file_name);
                             files.push((path, size_string.parse::<usize>().unwrap()));
                         }
@@ -107,18 +143,47 @@ fn main() {
                 while 0 < dir_stack.len() {
                     dir_stack.pop();
                 }
-                //current_dir = &mut root_dir;
             }
             _ => panic!("unknowwn command line: {}", command),
         };
-        //eprintln!("----\n{}", command);
     });
 
     files.sort();
+    files.insert(0, (String::from(""), 0));
 
+    /*
     for (path, size) in files.iter() {
         eprintln!("{:>9} : {}", size, path);
     }
+    */
     println!("A: {}", solve_a(&files));
-    //println!("B: {}", solve_b(&root));
+    println!("B: {}", solve_b(&files));
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    mod directory_sizes {
+        use super::*;
+
+        #[test]
+        fn should_sum_correctly_for_d() {
+            // Given
+            let input = vec![
+                (String::from("/d/d.ext"), 5626152),
+                (String::from("/d/d.log"), 8033020),
+                (String::from("/d/j"), 4060174),
+                (String::from("/d/k"), 7214296),
+            ];
+
+            // When
+            let result = directory_sizes(&input);
+
+            // Then
+            let expected = vec![(String::from("/d"), 24933642)];
+            assert_eq!(expected, result);
+        }
+    }
+}
+
